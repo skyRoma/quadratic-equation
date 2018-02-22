03590afcd821c7b9125181770cbe386cb7fd4fb3
roman <roma.sluka.97@mail.ru>
2018-02-22 03:43:09 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var mas = equation.split(' ');
+    a = mas[0];
+    if (mas[3] == '-') b = -mas[4];
+    else
+        b = mas[4];
+    if (mas[7] == '-') c = -mas[8];
+    else
+        c = mas[8];
+    var d = Math.pow(b, 2) - 4 * a * c
+    var x1 = Math.round((-1 * b + Math.sqrt(d)) / (2 * a));
+    var x2 = Math.round((-1 * b - Math.sqrt(d)) / (2 * a));
+    var solutions = [x1, x2].sort(function compareNumeric(a, b) {
+        return a - b;
+    });
+    return solutions;
 }
